29bd0613733ab7535eda1aef21aa91083a6090d1
Litvinenko Alexandra <alexandralit09022005@gmail.com>
2024-09-24 19:59:04 +0300

    step 3

--- ball.py
+++ ball.py
@@ -1,73 +1,51 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((400, 300))
+screen = pygame.display.set_mode((300, 400))
 
 ball_1 = pygame.Rect(0, 0, 20, 20)
-rect = pygame.Rect(0, 250, 50, 10)
-# ball_2 = pygame.Rect(100, 100, 20, 20)
+rect = pygame.Rect(0, 350, 50, 12)
 
-dx_1 = 4
-dy_1 = 10
-
-dx_2 = 3
-dy_2 = 12
+dx_1 = 3
+dy_1 = 9
 
 done = False
-
 
 clock = pygame.time.Clock()
 
 while not done:
-    screen.fill((0, 0, 0))
+    screen.fill((213, 109, 11))
 
-    pygame.draw.ellipse(screen, 'Red', ball_1)
-    pygame.draw.rect(screen, 'Blue', rect)
-    # pygame.draw.ellipse(screen, 'Blue', ball_2)
+    pygame.draw.ellipse(screen, (0, 0, 0), ball_1)
+    pygame.draw.rect(screen, (176, 24, 0), rect)
+
     ball_1.x += dx_1
     ball_1.y += dy_1
-    # ball_2.x += dx_2
-    # ball_2.y += dy_2
 
-    # if ball_2.y >= 300:
-    #     dy_2 = -dy_2
-    # if ball_2.y <= 0:
-    #     dy_2 = abs(dy_2)
-    # if ball_2.x >= 400:
-    #     dx_2 = -dx_2
-    # if ball_2.x <= 0:
-    #     dx_2 = abs(dx_2)
-
-    if ball_1.y >= 300:
+    if ball_1.y >= 400:
         dy_1 = -dy_1
     if ball_1.y <= 0:
         dy_1 = abs(dy_1)
-    if ball_1.x >= 400:
+    if ball_1.x >= 300:
         dx_1 = -dx_1
     if ball_1.x <= 0:
         dx_1 = abs(dx_1)
 
     if ball_1.colliderect(rect):
         dy_1 = -dy_1
-        dx_1 = -dx_1
-        # dy_2 = -dy_2
-        # dx_2 = -dx_2
+
+    keys = pygame.key.get_pressed()
+    if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and rect.x - 7 >= 0:
+        rect.x -= 7
+
+    if (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and rect.x + 50 <= 300:
+        rect.x += 7
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d and rect.x + 50 <= 400 - 10:
-                screen.fill((0, 0, 0))
-                rect.x += 10
-            if event.key == pygame.K_a and rect.x - 10 >= 0:
-                screen.fill((0, 0, 0))
-                rect.x -= 10
 
     pygame.display.flip()
     clock.tick(60)
 
     pygame.display.flip()
-
-
-# colliderect
