880aa4726088a8d5d4da4b9826237a975247edce
Litvinenko Alexandra <alexandralit09022005@gmail.com>
2024-09-28 23:00:22 +0300

    step 8

--- ball.py
+++ ball.py
@@ -7,13 +7,23 @@ screen = pygame.display.set_mode((300, 400))
 lives = 5
 font = pygame.font.Font(None, 36)
 
-ball_1 = pygame.Rect(0, 140, 20, 20)
+ball = pygame.Rect(0, 140, 20, 20)
 rect = pygame.Rect(0, 350, 50, 12)
 
-bonus_active_time = 0
 bonus_duration = 7000
-bonus_ball_active = False
-bonus_ball = pygame.Rect(150, 110, 15, 15)
+
+bonus_ball_1 = pygame.Rect(150, 110, 15, 15)
+bonus_1_active_time = 0
+bonus_ball_1_active = False
+
+bonus_ball_2 = pygame.Rect(150, 110, 15, 15)
+bonus_ball_2_active = False
+
+bonus_ball_3 = pygame.Rect(150, 110, 15, 15)
+bonus_ball_3_active = False
+
+bonus_ball_4 = pygame.Rect(150, 110, 15, 15)
+bonus_ball_4_active = False
 
 blocks = []
 coord = [5, 25]
@@ -25,11 +35,10 @@ for i in range(20):
         coord[1] += 25
     blocks.append(pygame.Rect(*coord, 50, 20))
 
-dx_1 = 3
-dy_1 = 9
+dx = 3
+dy = 9
 
 done = False
-start_time = pygame.time.get_ticks()
 clock = pygame.time.Clock()
 
 while not done:
@@ -46,49 +55,92 @@ while not done:
     for block in blocks:
         pygame.draw.rect(screen, 'White', block)
 
-    pygame.draw.ellipse(screen, (0, 0, 0), ball_1)
+    pygame.draw.ellipse(screen, (0, 0, 0), ball)
     pygame.draw.rect(screen, 'White', rect)
 
-    curr_time = pygame.time.get_ticks()
-    if curr_time - bonus_active_time > bonus_duration and rect.width > 50:
+    if pygame.time.get_ticks() - bonus_1_active_time > bonus_duration and rect.width > 50:
         rect.width = 50
 
-    if bonus_ball_active:
-        pygame.draw.ellipse(screen, 'Green', bonus_ball)
-        bonus_ball.y += 10
-        if rect.colliderect(bonus_ball):
+    if bonus_ball_1_active:
+        pygame.draw.ellipse(screen, (12, 243, 91), bonus_ball_1)
+        bonus_ball_1.y += 7
+        if rect.colliderect(bonus_ball_1):
             rect.width = 100
-            bonus_active_time = curr_time
-            bonus_ball_active = False
-        elif bonus_ball.y > 400:
-            bonus_ball_active = False
+            bonus_1_active_time = pygame.time.get_ticks()
+            bonus_ball_1_active = False
+        elif bonus_ball_1.y > 400:
+            bonus_ball_1_active = False
 
-    ball_1.x += dx_1
-    ball_1.y += dy_1
+    if bonus_ball_2_active:
+        pygame.draw.ellipse(screen, (92, 189, 0), bonus_ball_2)
+        bonus_ball_2.y += 7
+        if rect.colliderect(bonus_ball_2):
+            lives += 1
+            bonus_ball_2_active = False
+        elif bonus_ball_2.y > 400:
+            bonus_ball_2_active = False
 
-    if ball_1.y >= 400:
-        ball_1.x = 0
-        ball_1.y = 140
+    if bonus_ball_3_active:
+        pygame.draw.ellipse(screen, (180, 24, 9), bonus_ball_3)
+        bonus_ball_3.y += 7
+        if rect.colliderect(bonus_ball_3):
+            if dy > 0:
+                dy += 2
+            else:
+                dy -= 2
+            bonus_ball_3_active = False
+        elif bonus_ball_3.y > 400:
+            bonus_ball_3_active = False
+
+    if bonus_ball_4_active:
+        pygame.draw.ellipse(screen, (180, 24, 9), bonus_ball_4)
+        bonus_ball_4.y += 7
+        if rect.colliderect(bonus_ball_4):
+            ball.width -= 8
+            ball.height -= 8
+            bonus_ball_4_active = False
+        elif bonus_ball_4.y > 400:
+            bonus_ball_4_active = False
+
+    ball.x += dx
+    ball.y += dy
+
+    if ball.y >= 400:
+        ball.x = 0
+        ball.y = 140
         lives -= 1
-    if ball_1.y <= 0:
-        dy_1 = abs(dy_1)
-    if ball_1.x >= 300:
-        dx_1 = -dx_1
-    if ball_1.x <= 0:
-        dx_1 = abs(dx_1)
+    if ball.y <= 0:
+        dy = abs(dy)
+    if ball.x >= 300:
+        dx = -dx
+    if ball.x <= 0:
+        dx = abs(dx)
 
-    if ball_1.colliderect(rect):
-        dy_1 = -dy_1
-        ball_1.y = rect.y - ball_1.height
+    if ball.colliderect(rect):
+        dy = -dy
+        ball.y = rect.y - ball.height
 
     for i in range(len(blocks)):
-        if i < len(blocks) and ball_1.colliderect(blocks[i]):
-            if not bonus_ball_active and random.randint(2, 2) == 2:
-                bonus_ball_active = True
-                bonus_ball.x = ball_1.x
-                bonus_ball.y = ball_1.y
+        if ball.colliderect(blocks[i]):
+            rnd = random.randint(1, 10)
+            if not bonus_ball_1_active and rnd == 1:
+                bonus_ball_1_active = True
+                bonus_ball_1.x = ball.x
+                bonus_ball_1.y = ball.y
+            if not bonus_ball_2_active and rnd == 2:
+                bonus_ball_2_active = True
+                bonus_ball_2.x = ball.x
+                bonus_ball_2.y = ball.y
+            if not bonus_ball_3_active and rnd == 3:
+                bonus_ball_3_active = True
+                bonus_ball_3.x = ball.x
+                bonus_ball_3.y = ball.y
+            if not bonus_ball_4_active and rnd == 4:
+                bonus_ball_4_active = True
+                bonus_ball_4.x = ball.x
+                bonus_ball_4.y = ball.y
             blocks.pop(i)
-            dy_1 = -dy_1
+            dy = -dy
             break
 
     keys = pygame.key.get_pressed()
